c3e1086ac659f0cdb2e966618ee3adb3a3f3d05e
mw138 <mattheww138@gmail.com>
2022-05-27 11:11:28 +0100

    Update server.lua

--- example/server.lua
+++ example/server.lua
@@ -7,45 +7,51 @@ AddEventHandler('playerConnecting', function(pName, pKickReason, pDeferrals)
 
     Wait(1000)
     
-    while true do
-        local card = DeferralCards.Card:Create({
-            body = {
-                DeferralCards.Container:Create({
-                    items = {
-                        DeferralCards.CardElement:Image({
-                            url = 'https://logos-world.net/wp-content/uploads/2021/03/FiveM-Logo.png',
-                            size = 'large',
-                            horizontalAlignment = 'center'
-                        }),
-                        DeferralCards.CardElement:TextBlock({
-                            text = ('Welcome, %s'):format(pName),
-                            weight = 'Light',
-                            size = 'large',
-                            horizontalAlignment = 'center'
-                        }),
-                        DeferralCards.Container:ActionSet({
-                            actions = {
-                                DeferralCards.Action:Submit({
-                                    id = 'submit_join',
-                                    title = 'Join'
-                                })
-                            }
-                        })
-                    },
-                    isVisible = true
-                })
-            }
-        })
+    CreateThread(function()
+        local breakLoop = false
+        while true do
+            local card = DeferralCards.Card:Create({
+                body = {
+                    DeferralCards.Container:Create({
+                        items = {
+                            DeferralCards.CardElement:Image({
+                                url = 'https://logos-world.net/wp-content/uploads/2021/03/FiveM-Logo.png',
+                                size = 'large',
+                                horizontalAlignment = 'center'
+                            }),
+                            DeferralCards.CardElement:TextBlock({
+                                text = ('Welcome, %s'):format(pName),
+                                weight = 'Light',
+                                size = 'large',
+                                horizontalAlignment = 'center'
+                            }),
+                            DeferralCards.Container:ActionSet({
+                                actions = {
+                                    DeferralCards.Action:Submit({
+                                        id = 'submit_join',
+                                        title = 'Join'
+                                    })
+                                }
+                            })
+                        },
+                        isVisible = true
+                    })
+                }
+            })
+    
+            pDeferrals.presentCard(card, function(pData, pRawData)
+                if pData.submitId == 'submit_join' then
+                    pDeferrals.update('✅ Connecting to server...')
+                    Wait(1000)
+                    pDeferrals.done()
+                    print(('%s is connecting to the server'):format(pName))
+                    breakLoop = true
+                end
+            end)
 
-        pDeferrals.presentCard(card, function(pData, pRawData)
-            if pData.submitId == 'submit_join' then
-                pDeferrals.update('✅ Connecting to server...')
-                Wait(1000)
-                pDeferrals.done()
-                print(('%s is connecting to the server'):format(pName))
-            end
-        end)
-
-        Wait(1000)
-    end
+            if breakLoop then break end
+    
+            Wait(1000)
+        end
+    end)
 end)
